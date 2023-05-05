a9cf0e3a1842bb72ffba85d321578154d36aa974
Ecem <tugbaecemy@gmail.com>
2023-05-05 15:25:25 +0300

    Color changed end

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -6,15 +6,40 @@ public class ChangeColor : MonoBehaviour
 {
     public Color[] colors;
 
-    Color ilkRenk, ikinciRenk;
+    Color ikinciRenk;
     int birinciRenk;
     public Material zeminMat;
 
     private void Start()
     {
         birinciRenk = Random.Range(0, colors.Length);
+        ikinciRenk = colors[IkinciRenkSec()];
         zeminMat.color = colors[birinciRenk];
         Camera.main.backgroundColor = colors[birinciRenk];
+    }
+
+    private void Update()
+    {
+        Color fark = zeminMat.color - ikinciRenk;
+        if (Mathf.Abs(fark.r)+Mathf.Abs(fark.g)+Mathf.Abs(fark.b)<0.2f)
+        {
+            ikinciRenk = colors[IkinciRenkSec()];
+        }
+        zeminMat.color = Color.Lerp(zeminMat.color,ikinciRenk,0.0003f);
+        Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, ikinciRenk, 0.0007f);
+    }
+
+    int IkinciRenkSec()
+    {
+        int ikinciRenkIndex;
+        ikinciRenkIndex = Random.Range(0, colors.Length);
+
+        while (ikinciRenkIndex==birinciRenk)
+        {
+            ikinciRenkIndex = Random.Range(0, colors.Length);
+        }
+
+        return ikinciRenkIndex;
     }
 
 }//class
