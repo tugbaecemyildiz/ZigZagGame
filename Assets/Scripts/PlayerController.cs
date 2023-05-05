50f0344c2dded6d701c2e784030e1ab33675f4a9
Ecem <tugbaecemy@gmail.com>
2023-05-05 10:11:59 +0300

    Player Mover

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 yon = Vector3.left;
+    [SerializeField] float speed;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (yon.x==0) //z ekseninde hareket ediyor demektir
+            {
+                yon = Vector3.left;
+            }
+            else
+            {
+                yon = Vector3.back;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector3 hareket = yon * speed * Time.deltaTime; //objemizin hareket değeri
+        transform.position += hareket; //hareket değerini sürekli pozisyonuma ekle
+    }
+
+
+
+
+
+
+}//class
